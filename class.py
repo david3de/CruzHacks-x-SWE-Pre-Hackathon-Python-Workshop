54cb2afc9d3448d2828f143e530e4b2d11995543
David Lang <david3de@gmail.com>
2018-12-01 09:12:25 -0800

    Hotfix, added workshop slides

--- class.py
+++ class.py
@@ -2,13 +2,13 @@
 class Student:
 	ucsc_student = True                      # Defined by default
 
-	def __init__(self, user_name, user_age): # The constructor. Must be named __init__, and must have self as an argument
+	def __init__(self, user_name, user_age): # The constructor. Must be named __init__, and all class functions must have self as an argument
 		self.name = user_name
 		self.age = user_age
 
 	
 	def birthday(self):                      # Simple class function that increments a student's age by one
-		self.age += 1                        # Any time we want to access a class object's variables, we must include self in the arguments
+		self.age += 1
 
 jon_chong = Student('Jon Chong', 21)
 print(jon_chong.name, jon_chong.age)
